146942e1a5e082690cd642b84468964343e1dde7
GiovannePDS7 <giovanne3282@gmail.com>
2025-05-06 11:37:34 -0300

    add: Login back/front-end e desenvolvendo tela inicial

---
+++ public/JS/sessao.js
@@ -0,0 +1,27 @@
+function validarSessao() {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+    var [primeiroNome] = nome.split(' ');
+
+    var b_usuario = document.getElementById("b_usuario");
+
+    if (email != null && nome != null) {
+        b_usuario.innerHTML = primeiroNome;
+    } else {
+        window.location = "../../Pages/Login/login.html";
+    }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../Pages/Login/login.html";
+}
+
+function verificarSessao() {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+
+    if (email != null && nome != null){
+        window.location = "../../Pages/Inicial/inicial.html";
+    }
+}
